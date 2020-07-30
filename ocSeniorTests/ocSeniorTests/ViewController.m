3d267e2b97a0f3b62655dea91f20ebd757554aaf
myl <myldast1@126.com>
2020-07-30 14:38:54 +0800

    autoreleasepool

--- ocSeniorTests/ocSeniorTests/ViewController.m
+++ ocSeniorTests/ocSeniorTests/ViewController.m
@@ -16,7 +16,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    [self autoreleasepoolExplain];
+}
+
+/** 1.
+ Important
+If you use Automatic Reference Counting (ARC), you cannot use autorelease pools directly. Instead, you use @autoreleasepool blocks. For example, in place of:
+
+NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+// Code benefitting from a local autorelease pool.
+[pool release];
+
+you would write:
+
+@autoreleasepool {
+    // Code benefitting from a local autorelease pool.
+}
+
+@autoreleasepool blocks are more efficient than using an instance of NSAutoreleasePool directly; you can also use them even if you do not use ARC.
+ */
+- (void)autoreleasepoolExplain{
+    
 }
 
 
