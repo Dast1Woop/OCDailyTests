09b41805875eecce7618c5f6c5b86964112427a0
myl <myldast1@126.com>
2021-08-12 17:44:16 +0800

    必须注意定时器可能导致的对 vc 的强引用

--- TestMainAsync/TestMainAsync/SecViewController.m
+++ TestMainAsync/TestMainAsync/SecViewController.m
@@ -8,7 +8,7 @@
 #import "SecViewController.h"
 
 @interface SecViewController ()
-
+@property (nonatomic,strong) NSNumber *number;
 @end
 
 @implementation SecViewController
@@ -16,14 +16,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.number = @5;
     
-//    [NSTimer scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+    __weak typeof(self) weakself = self;
+    [NSTimer scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+    
+        //weak-strong dance，避免了对 vc 的强引用
+        __strong typeof(self) strongself = weakself;
+        if (strongself) {
+            NSLog(@"haha:%@", strongself.number);
+        }
+        
+        //runloop->timer->self，导致对 vc 的强引用，vc无法释放，必须用weak-strong dance
+//        NSLog(@"haha:%@", self.number);
 //        NSLog(@"haha");
-//    }];
+    }];
     
     //repeats If YES, the timer will repeatedly reschedule itself until invalidated. If NO, the timer will be invalidated after it fires.
     //Stops the timer from ever firing again and requests its removal from its run loop.  - (void)invalidate;
-    [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(timer) userInfo:nil repeats:false];
+//    [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(timer) userInfo:nil repeats:false];
 }
 
 - (void)timer{
