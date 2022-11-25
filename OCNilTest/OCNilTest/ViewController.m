fe6f4cc50cba9172ae5cfe8c7e8116a05f72f94c
mayl <mayl@huatugz.com>
2022-11-25 17:56:30 +0800

    new

--- OCNilTest/OCNilTest/ViewController.m
+++ OCNilTest/OCNilTest/ViewController.m
@@ -19,22 +19,31 @@
     NSNumber *lNum = nil;
     NSString *lStr = nil;
     
+    //num if nil
     if (lNum) {
         NSLog(@"num if not nil");
     }else{
         NSLog(@"num if nil");
     }
     
+    //lStr is nil
     if (lStr) {
         NSLog(@"lStr is not nil");
     }else{
         NSLog(@"lStr is nil");
     }
     
+    //0
     NSLog(@"str len = %lu", (unsigned long)lStr.length);
+    
+    //lst len == 0
     if (0 == lStr.length) {
         NSLog(@"lst len == 0");
     }
+    
+    NSArray *arr = nil;
+    //0%lu(unsigned long)
+    NSLog(@"arr.count==%lu", arr.count);
 }
 
 
