2d5039e14a6917ca46df53571abe78e3022c90a4
myl <myldast1@126.com>
2023-01-08 02:05:03 +0800

    new

--- WeakTest/WeakTest/ViewController.m
+++ WeakTest/WeakTest/ViewController.m
@@ -9,6 +9,8 @@
 
 @interface ViewController ()
 
+@property(nonatomic, strong) UIView *testV;
+
 @end
 
 @implementation ViewController
@@ -16,6 +18,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:self.testV];
+    self.testV.frame = self.view.bounds;
+}
+
+- (UIView *)testV{
+    if (nil == _testV) {
+        _testV = [[UIView alloc] init];
+        _testV.backgroundColor = [UIColor orangeColor];
+    }
+    return  _testV;
 }
 
 
